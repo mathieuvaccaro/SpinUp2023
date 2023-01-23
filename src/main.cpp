df4c044df4b22ef24b626dd9981ab8334e8cdcdb
Mathieu Vaccaro <mathieu.vaccaro3@gmail.com>
2023-01-23 18:41:20 +0100

    tt

--- src/main.cpp
+++ src/main.cpp
@@ -105,6 +105,22 @@ void competition_initialize() {}
  */
 void autonomous() {
 
+/** PREMIERE VERSION : Les noms des attributs/Méthodes peuvent être ammenées a être changés
+ * Autonome ==> Scripté
+ * 
+ *  
+ * Fonctions :
+ *  - D_Avant(Distance) : Avancer d'une distance donné
+ * 	- D_Arriere(Distance) : Reculer d'une distance donné
+ * 	- R_Droite(Angle) : Rotation d'un angle donné
+ *  - R_Gauche(Angle) : Rotation d'un angle donné
+ *  - Tirer : Tire les Vexs
+ * 
+ * Distance (m)
+ * Angle (°)
+ **/
+
+
 }
 
 /**
